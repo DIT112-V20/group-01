1b65e7708507172b1dc75160c0c1a405922ecf01
Karimab <karimab@chalmers.se>
2020-05-01 23:05:02 +0200

    Files for android studio

---
+++ Android app/CarController/app/src/main/java/com/example/carcontroller/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.carcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        moveToNextPage();
+    }
+
+    private void moveToNextPage(){
+        Button nButton = (Button) findViewById(R.id.manuButton);
+        nButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, manualPage.class));
+            }
+        });
+
+
+    }
+}
