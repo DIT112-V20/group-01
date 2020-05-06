06519e264761830dd15fb89fa120a82c788f97bb
karamkhatib <62644049+karamkhatib@users.noreply.github.com>
2020-05-06 23:53:20 +0200

    Update manualCon.ino

--- manualCon/manualCon.ino
+++ manualCon/manualCon.ino
@@ -93,7 +93,7 @@ void handleInput(){
     car.setSpeed(fSpeed);
     car.setAngle(rDegrees);
   }
-    }
+    
   if (message == "backwards") { // go back
     car.setSpeed(bSpeed);
     car.setAngle(0);
