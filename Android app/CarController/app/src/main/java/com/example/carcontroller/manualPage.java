dc9f760ed0f94c4307aa204e6601cc9323e94dd4
Karimab <karimab@chalmers.se>
2020-05-09 22:20:56 +0200

    last version of the app

--- Android app/CarController/app/src/main/java/com/example/carcontroller/manualPage.java
+++ Android app/CarController/app/src/main/java/com/example/carcontroller/manualPage.java
@@ -1,14 +1,191 @@
 package com.example.carcontroller;
 
+
+//import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class manualPage extends AppCompatActivity {
 
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+    final DatabaseReference fwdStatus = myRef.child("manual").child("forward");
+    final DatabaseReference bwdStatus = myRef.child("manual").child("backward");
+    final DatabaseReference rightStatus = myRef.child("manual").child("right");
+    final DatabaseReference leftStatus = myRef.child("manual").child("left");
+
+
+
+
+
+    Button fwdButton;
+    Button bwdButton;
+    Button rightButton;
+    Button leftButton;
+
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_page);
+
+        fwdButton = (Button)findViewById(R.id.fwdButton);
+        bwdButton = (Button)findViewById(R.id.bwdButton);
+        rightButton = (Button)findViewById(R.id.rightButton);
+        leftButton = (Button)findViewById(R.id.leftButton);
+
+
+        //updateText(ledstatus1,textView1);
+
+        fwdStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+        bwdStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+        rightStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        leftStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        fwdButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    fwdStatus.setValue("off");
+
+                    return true;
+                }else{
+                    fwdStatus.setValue("on");
+                }
+                return false;
+            }
+
+
+        });
+
+        bwdButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    bwdStatus.setValue("off");
+
+                    return true;
+                }else{
+                    bwdStatus.setValue("on");
+                }
+                return false;
+            }
+
+
+        });
+
+        rightButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    rightStatus.setValue("off");
+
+                    return true;
+                }else{
+                    rightStatus.setValue("on");
+                }
+                return false;
+            }
+
+
+        });
+
+        leftButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    leftStatus.setValue("off");
+
+                    return true;
+                }else{
+                    leftStatus.setValue("on");
+                }
+                return false;
+            }
+
+
+        });
+
     }
 }
