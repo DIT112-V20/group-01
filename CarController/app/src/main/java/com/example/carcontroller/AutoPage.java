f76da3c8feefaa8ea95bb5ea29711e80cac4a82f
luai <luai@student.chalmers.se>
2020-05-23 13:44:13 +0200

    Testing new features

---
+++ CarController/app/src/main/java/com/example/carcontroller/AutoPage.java
@@ -0,0 +1,95 @@
+package com.example.carcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class AutoPage extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+    final DatabaseReference autoStatus = myRef.child("auto").child("mode");
+    final DatabaseReference distanceStatus = myRef.child("auto").child("covered_dis");
+
+    float distance_val;
+    ToggleButton autoMode;
+    TextView distance;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_page);
+        autoMode= findViewById(R.id.auto_mode);
+        distance = findViewById(R.id.distance);
+
+        autoStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        distanceStatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                distance_val = dataSnapshot.getValue(float.class);
+
+                Log.d("file", "Value is: " + distance_val);
+
+            }
+
+
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+        autoMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    autoStatus.setValue("on");
+                    distance.setText(""+distance_val);
+
+                }else {
+                    autoStatus.setValue("off");
+                }
+            }
+        });
+
+
+    }
+}
